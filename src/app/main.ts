34b092432d06066807b29785e0cf8920f1b44891
Wenderson Pires da Silva <wpdas@yahoo.com.br>
2017-04-17 03:12:11 -0300

    Modo: Produção (Angular)
    Atualização da especificação do Mode (Angular)

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import { enableProdMode } from '@angular/core';
+
 import { AppModule } from './app.module';
+
+//Define como modo producao
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
